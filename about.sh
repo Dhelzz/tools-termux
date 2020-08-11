fd75f7d3980dda1caf164ce2e146ec509ba0bada
famudz <57311772+famudz123@users.noreply.github.com>
2020-08-11 11:06:53 +0700

    Create about.sh

---
+++ about.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+echo "\033[1;34m"
+clear 
+echo "==========================================="
+sleep 2
+echo "Dibuat oleh Fa???? Mu??????? Dz????,"
+sleep 2
+echo "Nama Masih Dirahasiakan."
+sleep 2
+echo "SUBSCRIBE sampe 100rb subs kalo mau tau,"
+sleep 2
+echo "saya siapa."
+sleep 2
+echo "link channel :"
+sleep 2
+echo "www.youtube.com/channel/UCsOXhwrK9iiXbXivPnnWGkg"
+sleep 2
+echo "atau link yang telah di singkat :"
+sleep 2
+echo "www.s.id/subs-ch"
+sleep 2
+echo ""
+echo ""
+echo "nama : Fa???? Mu??????? Dz????"
+echo "youtube : Famudz TV"
+echo "Github  : https://github.com/Famudz123"
+echo "==========================================="
+sleep 5
+clear
+
+
+
+
