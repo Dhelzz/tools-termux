f8b5d74d0705299266d152ac791a4bf761905141
Famudz TV <57311772+famudz123@users.noreply.github.com>
2020-08-11 17:41:07 +0700

    Tools termux

--- tools.sh
+++ tools.sh
@@ -116,7 +116,7 @@ then
     clear
     sleep 1
 else
-    echo "\033[1;31mjawaban tidak tersedia"
+    echo "\033[1;31mJawaban $pilih tidak tersedia tuan $nama"
     echo "\033[1;37m"
     sleep 3
     clear 
